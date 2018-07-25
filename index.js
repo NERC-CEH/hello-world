7fc405efbc30ea678d9313526ce164c933b19b14
Luke Preston <lukep@ceh.ac.uk>
2018-07-25 10:23:21 +0100

    adding env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(`Hello ${process.env.NAME}!`)
 })
 
 app.listen(80, function () {
